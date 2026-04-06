792a182f2684c80af571557bb1bf8783a767c609
Vinicius Tsuda <viniciustsuda9@gmail.com>
2026-04-06 16:32:38 -0300

    mais aula sobre api

--- Secao 9 - Node.js/aula_express/src/servidor.js
+++ Secao 9 - Node.js/aula_express/src/servidor.js
@@ -1,25 +1,47 @@
-const bd = require('./bd.js')
 const port = 3003
+const bd = require('./bd.js')
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.urlencoded({extended: true}))
 
+//lista produtos
 app.get('/produtos', (req, res) => {
     res.send(bd.getProdutos())
 })
 
+//lista um produto com id na url
 app.get('/produtos/:id', (req, res) => {
     res.send(bd.getProdutos(req.params.id))
 })
 
+//criar produto
 app.post('/produtos', (req, res) => {
     const produto = bd.salvarProduto ({
-        nome: req.body.name,
+        nome: req.body.nome,
         preco: req.body.preco
     })
-    res.send(produto)
+    res.send(produto) //JSON
 }) 
+
+//update de um produto
+app.put('/produtos/:id', (req, res) => {
+    const produto = bd.salvarProduto ({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) //JSON
+}) 
+
+//deletar produto
+app.delete('/produtos/:id', (req, res) => {
+    const produto = bd.excluirProduto(req.params.id)
+    res.send(produto) //JSON
+}) 
+
 
 app.listen(port, () => {
     console.log(`Servidor executando na porta ${port}.`)
