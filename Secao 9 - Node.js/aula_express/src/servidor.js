db5faff957c012d384114d6032970ac548b5b6d4
Vinicius Tsuda <viniciustsuda9@gmail.com>
2026-04-06 16:03:32 -0300

    mais estudo

--- Secao 9 - Node.js/aula_express/src/servidor.js
+++ Secao 9 - Node.js/aula_express/src/servidor.js
@@ -1,15 +1,25 @@
+const bd = require('./bd.js')
 const port = 3003
+
 const express = require('express')
 const app = express()
 
-app.get('/produtos', (req, res) => {
-    console.log('Middleware 1...')
-    next()
-})
 
 app.get('/produtos', (req, res) => {
-    res.send({nome: 'Notebook', preco: 123.45}) // vai converter para json
+    res.send(bd.getProdutos())
 })
+
+app.get('/produtos/:id', (req, res) => {
+    res.send(bd.getProdutos(req.params.id))
+})
+
+app.post('/produtos', (req, res) => {
+    const produto = bd.salvarProduto ({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto)
+}) 
 
 app.listen(port, () => {
     console.log(`Servidor executando na porta ${port}.`)
